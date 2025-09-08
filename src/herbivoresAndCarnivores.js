55f84eb90cbd4f302b666ef8ffe6ab22e42074b2
renatoanchieta <renatovalerio@gmail.com>
2025-09-08 11:43:01 -0300

    update 4

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,58 +1,47 @@
 'use strict';
 
-export class Animal {
-    constructor(name, health = 100) {
-        this.name = name;
-        this._health = health;
-        if (this._health > 0) {
-            Animal.alive.push(this);
-        }
+class Animal {
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    // Adiciona o animal à lista de vivos
+    Animal.alive.push(this);
+  }
+
+  // Remove da lista se morrer
+  _checkHealth() {
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== this);
     }
-
-    static alive = [];
-
-    set health(value) {
-        this._health = value;
-        if (this._health <= 0) {
-            this.die();
-        }
-    }
-
-    static alive = [];
-
-    set health(value) {
-        this._health = value;
-        if (this._health <= 0) {
-            this.die();
-        }
-    }
-
-    get health() {
-        return this._health;
-    }
-
-    die() {
-        Animal.alive = Animal.alive.filter(animal => animal.health > 0);
-    }
+  }
 }
 
-export class Herbivore extends Animal {
-    constructor(name, health) {
-        super(name, health);
-        this.hidden = false;
-    }
+class Herbivore extends Animal {
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
 
-    hide() {
-        this.hidden = true;
-    }
+  hide() {
+    this.hidden = true;
+  }
 }
 
-export class Carnivore extends Animal {
-    bite(herbivore) {
-        if (herbivore instanceof Herbivore && !herbivore.hidden) {
-            herbivore.health -= 50;
-        }
+class Carnivore extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  bite(target) {
+    // Só pode morder herbívoros não escondidos
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+      target._checkHealth();
     }
+  }
 }
 
 module.exports = {
