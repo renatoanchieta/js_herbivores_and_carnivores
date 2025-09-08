f3e28490933e452df1e03b71e1d7a6764ce83375
renatoanchieta <renatovalerio@gmail.com>
2025-09-08 11:26:51 -0300

    change in js file

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,54 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+        this.name = name;
+        this.health = health;
+        Animal.alive.push(this);
+    }
+
+    static alive = [];
+
+    set health(value) {
+        this._health = value;
+        if (this._health <= 0) {
+            this.die();
+        }
+    }
+
+    get health() {
+        return this._health;
+    }
+
+    die() {
+        const index = Animal.alive.indexOf(this);
+        if (index > -1) {
+            Animal.alive.splice(index, 1);
+        }
+    }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+      constructor(name, health) {
+        super(name, health);
+        this.hidden = false;
+    }
+
+    hide() {
+        this.hidden = true;
+    }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+      constructor(name, health) {
+        super(name, health);
+    }
+
+    bite(herbivore) {
+        if (herbivore instanceof Herbivore && !herbivore.hidden) {
+            herbivore.health -= 50;
+        }
+    }
 }
 
 module.exports = {
